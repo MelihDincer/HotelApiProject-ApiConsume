56aa537e86cd29a604d4dfeb4b37fc180dc14232
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-16 21:24:07 +0300

    EntityFramework klasörü tanımlandı.

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfSubscribeDal.cs
@@ -0,0 +1,20 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfSubscribeDal : GenericRepository<Subscribe>, ISubscribeDal //ISubscribeDal'ı da eklememizin sebebi entity'e özgü metotların eklenecek olması.
+    {
+        public EfSubscribeDal(Context context) : base(context)
+        {
+
+        }
+    }
+}
