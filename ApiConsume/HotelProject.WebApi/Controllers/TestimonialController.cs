5e73bf96e655631d94febc173061cc1f745ef6df
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-17 14:22:27 +0300

    Api Controller oluşturma ve CRUD işlemleri

---
+++ ApiConsume/HotelProject.WebApi/Controllers/TestimonialController.cs
@@ -0,0 +1,54 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService _testimonialService;
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var values = _testimonialService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonial Testimonial)
+        {
+            _testimonialService.TInsert(Testimonial);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = _testimonialService.TGetByID(id);
+            _testimonialService.TDelete(value);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            _testimonialService.TUpdate(testimonial);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var value = _testimonialService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
