ee9feca2f39cf3e1a6103eea69129c2624c4b6bc
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-17 14:23:14 +0300

    Servis ekleme işlemleri (Scoped örneği ve bağlantı)

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -1,8 +1,31 @@
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddScoped<IStaffDal, EfStaffDal>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
+
+builder.Services.AddScoped<IServicesDal, EfServiceDal>();
+builder.Services.AddScoped<IServiceService, ServiceManager>();
+
+builder.Services.AddScoped<IRoomDal, EfRoomDal>();
+builder.Services.AddScoped<IRoomService, RoomManager>();
+
+builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
