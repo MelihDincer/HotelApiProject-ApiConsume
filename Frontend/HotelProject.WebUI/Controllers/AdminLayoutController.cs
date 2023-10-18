4d6537d298e9598e6ef20f05752073ad89040fe0
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-18 14:06:21 +0300

    AdminLayoutController oluşturuldu. PartialViewları tanımlandı.

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+
+        public IActionResult HeadPartial()
+        {
+            return View();
+        }
+
+        public IActionResult PreloaderPartial()
+        {
+            return View();
+        }
+        public IActionResult NavheaderPartial()
+        {
+            return View();
+        }
+
+        public IActionResult HeaderPartial()
+        {
+            return View();
+        }
+        public IActionResult SidebarPartial()
+        {
+            return View();
+        }
+        public IActionResult FooterPartial()
+        {
+            return View();
+        }
+        public IActionResult ScriptPartial()
+        {
+            return View();
+        }
+    }
+}
