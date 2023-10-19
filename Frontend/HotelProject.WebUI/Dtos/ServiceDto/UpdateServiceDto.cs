16f81645f5927880374ace01df9022405fcde3b5
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-19 19:46:23 +0300

    Hizmet güncelleme işlemi için ilgili dto sınıfının eklenmesi.

---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+        public int ServiceID { get; set; }
+        [Required(ErrorMessage = "Hizmet ikon linki giriniz")]
+        public string? ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hizmet başlığını  giriniz")]
+        [StringLength(100, ErrorMessage = "Hizmet başlığı en fazla 100 karakter olabilir")]
+        public string? Title { get; set; }
+        [Required(ErrorMessage = "Hizmet açıklaması  giriniz")]
+        [StringLength(100, ErrorMessage = "Hizmet açıklaması en fazla 100 karakter olabilir")]
+        public string? Description { get; set; }
+    }
+}
