586e2c748129f25f1726955753f10197ff406941
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-19 19:39:25 +0300

    ServiceController oluşturulması. Crud işlemlerinin tamamlanması.

---
+++ Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,94 @@
+﻿using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        //API'ın Consume Edilmesi
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient(); //İstemci oluşturduk.
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/Service"); //Adrese istekte bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync(); //Gelen veri jsondata değişkenine aktarıldı. jsondata json türünde.
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsondata); //Tablomuza verinin aktarılması için deserialize işlemi yapıldı.
+                return View(values);
+            }
+            return View();
+        }
+        //End
+
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddService(CreateServiceDto createServiceDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createServiceDto); //gelen datayı(model) json a dönüştür.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:50430/api/Service", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:50430/api/Service?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:50430/api/Service/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); //Veri listeleme
+                var values = JsonConvert.DeserializeObject<UpdateServiceDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(UpdateServiceDto updateServiceDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateServiceDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:50430/api/Service/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
