8be5f8963ac63379afe3106e3e7b9ca1dc91d347
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-19 19:47:03 +0300

    Eklenen dto sınıflarının mapleme işlemlerinin tamamlanması.

---
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.LoginDto;
+using HotelProject.WebUI.Dtos.RegisterDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+
+namespace HotelProject.WebUI.Mapping
+{
+    public class AutoMapperConfig : Profile
+    {
+        //Mapleme işlemi
+        public AutoMapperConfig()
+        {
+            CreateMap<ResultServiceDto, Service>().ReverseMap(); //ReverseMap metodu sayesinde hem bu şekilde hem de tersine yani Service, ResultServiceDto şeklinde bir mapleme işlemi gerçekleştirilecektir. Aksi takdirde aşağıdaki örnekteki gibi 2 kez kod yazmamız gerekecekti.
+
+            //CreateMap<ResultServiceDto, Service>();
+            //CreateMap<Service, ResultServiceDto>();
+
+            CreateMap<UpdateServiceDto, Service>().ReverseMap();
+            CreateMap<CreateServiceDto, Service>().ReverseMap();
+
+            CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
+            CreateMap<LoginUserDto, AppUser>().ReverseMap();
+        }
+    }
+}
