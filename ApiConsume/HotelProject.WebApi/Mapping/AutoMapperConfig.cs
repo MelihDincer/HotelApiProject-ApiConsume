e6945e4056a1f28a5d5dcd48aa45d73e3156b61f
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-19 19:35:30 +0300

    AutoMapperConfig sınıfının oluşturulması ve ilgili sınıfların eşlenmesi.

---
+++ ApiConsume/HotelProject.WebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.WebApi.Mapping
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<RoomAddDto, Room>().ReverseMap();
+            CreateMap<RoomUpdateDto, Room>().ReverseMap();           
+        }
+    }
+}
