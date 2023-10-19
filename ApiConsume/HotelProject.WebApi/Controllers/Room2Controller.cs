a84ea7f28b42712d5c66e2cf759994cb5244f466
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-19 19:32:33 +0300

    Odaların Mapper Kontrolü ile Crud işlemlerinin yapılması işlemleri için Room2Controller oluşturulması.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/Room2Controller.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Room2Controller : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+
+        public Room2Controller(IRoomService roomService, IMapper mapper)
+        {
+            _roomService = roomService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = _roomService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddRoom(RoomAddDto roomAddDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var values = _mapper.Map<Room>(roomAddDto);
+            _roomService.TInsert(values);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateRoom(RoomUpdateDto roomUpdateDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var values = _mapper.Map<Room>(roomUpdateDto);
+            _roomService.TUpdate(values);
+            return Ok("Başarıyla Güncellendi");
+        }
+    }
+}
