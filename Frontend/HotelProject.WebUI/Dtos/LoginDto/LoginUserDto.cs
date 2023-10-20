2606aeebe6630dbb711a9cedf494bf4a3ec90fc4
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-20 18:07:42 +0300

    Login işlemi için gerekli dto sınıfı ve entitylerin oluşturulması.

---
+++ Frontend/HotelProject.WebUI/Dtos/LoginDto/LoginUserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.LoginDto
+{
+    public class LoginUserDto
+    {
+        [Required(ErrorMessage = "Kullanıcı Adı Alanı Zorunludur!")]
+        public string? Username { get; set; }
+        [Required(ErrorMessage = "Şifre Girmeniz Zorunludur!")]
+        public string? Password { get; set; }
+    }
+}
