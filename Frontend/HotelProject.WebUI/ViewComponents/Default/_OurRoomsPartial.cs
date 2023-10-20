8d041e7387591269edfaff2c4cd9be4bf4acceee
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-20 18:33:05 +0300

    OurRooms bölümünün tamamlanması. (ViewComponent class ve partialview in düzenlenmesi)

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.RoomDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _OurRoomsPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _OurRoomsPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient(); //İstemci oluşturduk.
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/Room"); //Adrese istekte bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync(); //Gelen veri jsondata değişkenine aktarıldı. jsondata json türünde.
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsondata); //Tablomuza verinin aktarılması için deserialize işlemi yapıldı.
+                return View(values);
+            }
             return View();
         }
     }
