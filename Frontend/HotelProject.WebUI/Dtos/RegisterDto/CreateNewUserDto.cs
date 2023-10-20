d46e2a53c52b9abf2391d0178765ae737297a9fd
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-20 18:08:19 +0300

    Kayıt olma işlemi için ilgili dto sınıfının ve entitylerin oluşturulması.

---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage = "Ad Alanı Zorunludur!")]
+        public string? Name{ get; set; }
+
+        [Required(ErrorMessage = "Soyad Alanı Zorunludur!")]
+        public string? Surname { get; set; }
+        [Required(ErrorMessage = "Kullanıcı Adı Alanı Zorunludur!")]
+        public string? Username { get; set; }
+        [Required(ErrorMessage = "Mail Alanı Zorunludur!")]
+        public string? Mail { get; set; }
+        [Required(ErrorMessage = "Şifre Alanı Zorunludur!")]
+        public string? Password { get; set; }
+        [Required(ErrorMessage = "Şifre Tekrar Alanı Zorunludur!")]
+        [Compare("Password",ErrorMessage = "Şifreler Uyuşmuyor")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
