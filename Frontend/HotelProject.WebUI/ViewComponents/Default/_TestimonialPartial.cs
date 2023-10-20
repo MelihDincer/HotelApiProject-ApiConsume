f8765bbe3569915458f048585ac419a18482c9c6
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-20 18:34:24 +0300

    Testimonial bölümünün tamamlanması. (ViewComponent class ve partialview in düzenlenmesi)

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.TestimonialDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _TestimonialPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient(); //İstemci oluşturduk.
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/Testimonial"); //Adrese istekte bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync(); //Gelen veri jsondata değişkenine aktarıldı. jsondata json türünde.
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsondata); //Tablomuza verinin aktarılması için deserialize işlemi yapıldı.
+                return View(values);
+            }
             return View();
         }
     }
