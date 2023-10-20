343307701763e4aaed952f2e9ea47574453f2163
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-20 17:52:25 +0300

    About tablosunun eklenmesi.

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -15,5 +15,6 @@ namespace HotelProject.DataAccessLayer.Concrete
         public DbSet<Staff> Staffs { get; set; }
         public DbSet<Subscribe> Subscribes { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<About> Abouts { get; set; }
     }
 }
