d7d482e203dc91e4c053f272a1999cef50149888
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-20 18:33:49 +0300

    TeamPartial bölümünün tamamlanması. (ViewComponent class ve partialview in düzenlenmesi)

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.StaffDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _TeamPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TeamPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient(); //İstemci oluşturduk.
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/Staff"); //Adrese istekte bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync(); //Gelen veri jsondata değişkenine aktarıldı. jsondata json türünde.
+                var values = JsonConvert.DeserializeObject<List<ResultStaffDto>>(jsondata); //Tablomuza verinin aktarılması için deserialize işlemi yapıldı.
+                return View(values);
+            }
             return View();
         }
     }
