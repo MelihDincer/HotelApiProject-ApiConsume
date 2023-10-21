3a5184b4c0b510147df92ce522fd3d514b0cd7a5
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-21 23:01:01 +0300

    Rezervasyon sayfası ve rezervasyon oluşturma işlemleri tamamlandı.

---
+++ Frontend/HotelProject.WebUI/Controllers/BookingController.cs
@@ -0,0 +1,42 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult AddBooking()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBooking(CreateBookingDto createBookingDto)
+        {
+            createBookingDto.Status = "Onay Bekliyor";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto); //gelen datayı(model) json a dönüştür.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:50430/api/Booking", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+    }
+}
