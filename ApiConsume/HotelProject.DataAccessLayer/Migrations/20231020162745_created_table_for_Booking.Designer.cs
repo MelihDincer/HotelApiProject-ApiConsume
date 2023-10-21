7521804c8b96545d134d57cff2c24968593f116e
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-21 18:46:30 +0300

    Booking entity si için migration oluşturulması.

--- ApiConsume/HotelProject.DataAccessLayer/Migrations/20231020162745_created_table_for_Booking.Designer.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20231020162745_created_table_for_Booking.Designer.cs
@@ -4,6 +4,7 @@ using HotelProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231020162745_created_table_for_Booking")]
+    partial class created_table_for_Booking
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
