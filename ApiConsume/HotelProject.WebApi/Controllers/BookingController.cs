17842dbd4cf074a27ba8e455c9223d1566cf65a5
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-21 18:49:54 +0300

    BookingController  API Controller'ın oluşturulması. CRUD işlemlerinin tamamlanması.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/BookingController.cs
@@ -0,0 +1,68 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var values = _bookingService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddBooking(Booking booking)
+        {
+            _bookingService.TInsert(booking);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBooking(int id)
+        {
+            var value = _bookingService.TGetByID(id);
+            _bookingService.TDelete(value);
+            return Ok();
+        }
+
+        [HttpPut("UpdateBooking")]
+        public IActionResult UpdateBooking(Booking booking)
+        {
+            _bookingService.TUpdate(booking);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBooking(int id)
+        {
+            var value = _bookingService.TGetByID(id);
+            return Ok(value);
+        }
+
+        [HttpPut("ApprovedBooking")]
+        public IActionResult ApprovedBooking(Booking booking)
+        {
+            _bookingService.TBookingStatusChangeApproved(booking);
+            return Ok();
+        }
+
+        [HttpPut("ApprovedBooking2")]
+        public IActionResult ApprovedBooking2(int id)
+        {
+            _bookingService.TBookingStatusChangeApproved2(id);
+            return Ok();
+        }
+    }
+}
