c091362a9d3732f0988050f3b1ff59abd6ecdb40
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-22 22:51:18 +0300

    Döviz Kur apisi ile Veri Çekme işleminin tamamlanması.

---
+++ RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "4989b69e0bmsh169542d11e74854p1241b2jsnf9e27d1a6ff4" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
+            }
+        }
+    }
+}
