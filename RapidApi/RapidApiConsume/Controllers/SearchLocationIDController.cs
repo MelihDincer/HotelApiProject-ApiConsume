8d0742c2bc5d6e680879da8454ba99a9088480c7
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-22 23:05:06 +0300

    Şehir ID Değeri Bulma Motoru sayfasının tamamlanması. Apide bulunan otellerin şehire göre listelenmesi için şehirlerin lokasyon id değerleri bu kısımda bulunmakta.

---
+++ RapidApi/RapidApiConsume/Controllers/SearchLocationIDController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class SearchLocationIDController : Controller
+    {
+        public async Task<IActionResult> Index(string cityName)
+        {
+            if(!string.IsNullOrEmpty(cityName))
+            {
+                List<BookingApiLocationSearchViewModel> model = new List<BookingApiLocationSearchViewModel>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={cityName}&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "4989b69e0bmsh169542d11e74854p1241b2jsnf9e27d1a6ff4" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<List<BookingApiLocationSearchViewModel>>(body);
+                    return View(model.Take(1).ToList());
+                }
+            }
+            else
+            {
+                List<BookingApiLocationSearchViewModel> model = new List<BookingApiLocationSearchViewModel>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=paris&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "4989b69e0bmsh169542d11e74854p1241b2jsnf9e27d1a6ff4" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<List<BookingApiLocationSearchViewModel>>(body);
+                    return View(model.Take(1).ToList());
+                }
+            }
+        }
+    }
+}
