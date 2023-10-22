c7c8305cb0c69156eabf9908b3510df2147da52e
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-22 22:44:58 +0300

    Imbdb film verilerinin api ile çekilmesi işlemi için controllerın oluşturulması.

---
+++ RapidApi/RapidApiConsume/Controllers/ImdbController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ImdbController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovieViewModels = new List<ApiMovieViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies1.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "4989b69e0bmsh169542d11e74854p1241b2jsnf9e27d1a6ff4" }, //İlgili API'a istekte bulunurken kullanılacak key.
+        { "X-RapidAPI-Host", "imdb-top-100-movies1.p.rapidapi.com" }, //Sağlayıcı linki
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovieViewModels = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+            }
+            return View(apiMovieViewModels);
+        }
+    }
+}
