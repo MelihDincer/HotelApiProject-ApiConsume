db0ed58a7b787e3c4dfd091afcc21bedda5a1781
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-23 14:29:56 +0300

    Ziyaretçi tablosunun eklenmesi.

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -17,5 +17,6 @@ namespace HotelProject.DataAccessLayer.Concrete
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<About> Abouts { get; set; }
         public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Guest> Guests { get; set; }
     }
 }
