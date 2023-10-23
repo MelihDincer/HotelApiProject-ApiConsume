acb6fc812bc9bad63ca1f1f538b4c2cb39820e40
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-23 22:54:23 +0300

    CreateToken class oluşturulması. Token oluşturma işlemlerinin tamamlanması. Admin-Normal Kullanıcı

---
+++ JwtProject/WebApiJwt/Models/CreateToken.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApiJwt.Models
+{
+    public class CreateToken
+    {
+        public string TokenCreate()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddSeconds(20), signingCredentials: credentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+
+        public string TokenCreateAdmin()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //Rollerin içeriği Claim de tutulacak.
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier,Guid.NewGuid().ToString()), //Burası olmasa da olur.
+                new Claim(ClaimTypes.Role,"Admin"),
+                new Claim(ClaimTypes.Role,"Visitor")
+            };
+
+            JwtSecurityToken jwtSecuritytoken = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddSeconds(20), signingCredentials: credentials, claims: claims);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(jwtSecuritytoken);
+        }
+    }
+}
