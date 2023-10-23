c4342026879c99df1d252a283d017979c0ebc24f
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-23 22:37:35 +0300

    ContactController'ın oluşturulması. Mesaj gönderme işleminin tamamlanması.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Contact contact)
+        {
+            contact.Date = Convert.ToDateTime(DateTime.Now.ToString());
+            _contactService.TInsert(contact);
+            return Ok();
+        }
+
+    }
+}
