ba9dc756c6bc58f76e8a250863196881e8de0a83
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-24 23:37:27 +0300

    FluentValidation kullanımı için program dosyasında gerekli konfigürasyon ayarlarının yapılması.

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -1,11 +1,19 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.GuestDto;
+using HotelProject.WebUI.ValidationRules.GuestValidationRules;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddHttpClient();
-builder.Services.AddControllersWithViews();
+
+//FluentValidation un kullanýlmasý için AddFluentValidation metodunun geçilmesi.
+builder.Services.AddTransient<IValidator<CreateGuestDto>, CreateGuestValidator>();
+builder.Services.AddTransient<IValidator<UpdateGuestDto>, UpdateGuestValidator>();
+builder.Services.AddControllersWithViews().AddFluentValidation(); 
 builder.Services.AddAutoMapper(typeof(Program)); //AutoMapper kullanýlmasý için burada belirtmemiz gerekti.
 builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
