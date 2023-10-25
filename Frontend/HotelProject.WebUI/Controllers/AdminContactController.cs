9219c44e7a7498523f827038ae6360cf0f681863
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-25 17:43:20 +0300

    AdminContactController'ın oluşturulması. Mesajlar bölümünün tamamlanması.

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -0,0 +1,110 @@
+﻿using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.SendMessageDto;
+using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminContactController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        //İletişim sayfasından gönderilen mesajların listelenmesi.
+        public async Task<IActionResult> Inbox()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/Contact");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        //Admin tarafından ziyaretçi veya kullanıcılara gönderilen mesajların listelenmesi.
+        public async Task<IActionResult> SendBox()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/SendMessage");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSendBoxDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddSendMessage()
+        {
+            return View();
+        }
+
+        //Admin tarafından mesaj gönderme işlemi
+        [HttpPost]
+        public async Task<IActionResult> AddSendMessage(CreateSendMessageDto createSendMessageDto)
+        {
+            createSendMessageDto.SenderMail = "admin@gmail.com";
+            createSendMessageDto.SenderName = "admin";
+            createSendMessageDto.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSendMessageDto); //gelen datayı(model) json a dönüştür.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:50430/api/SendMessage", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SendBox");
+            }
+            return View();
+        }
+        //Mesaj bölümünde soldaki gelen kutusunun olduğu kısım partialview
+        public PartialViewResult SidebarAdminContactPartial()
+        {
+            return PartialView();
+        }
+        //Mesaj bölümünde soldaki kategori bölümü partialview
+        public PartialViewResult SidebarAdminContactCategoryPartial()
+        {
+            return PartialView();
+        }
+
+        //Gönderilen Mesajların Listelenmesi
+        public async Task<IActionResult> MessageDetailsBySendbox(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:50430/api/SendMessage/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); //Veri listeleme
+                var values = JsonConvert.DeserializeObject<GetMessageByIDDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        //Contact Sayfasından Gelen Mesajların Listelenmesi
+        public async Task<IActionResult> MessageDetailsByInbox(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:50430/api/Contact/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); //Veri listeleme
+                var values = JsonConvert.DeserializeObject<InboxContactDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
