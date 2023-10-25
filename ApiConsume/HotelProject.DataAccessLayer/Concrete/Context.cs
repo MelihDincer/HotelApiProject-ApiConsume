1f0460fb07f438c36ecb1319cdbf5b212d3ad07d
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-25 17:30:08 +0300

    SendMessage sınıfının context sınıfında geçilmesi.

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -19,5 +19,6 @@ namespace HotelProject.DataAccessLayer.Concrete
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<Guest> Guests { get; set; }
         public DbSet<Contact> Contacts { get; set; }
+        public DbSet<SendMessage> SendMessages { get; set; }
     }
 }
