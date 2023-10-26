36f8b6eaee029013cfd8cff7b1796dc46c163a2f
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-26 22:53:31 +0300

    Dosya yükleme işlemleri için api kısmında FileImageController'ın oluşturulması ve resim yükleme işleminin yapılması.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/FileImageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileImageController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile file)
+        {
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName); //Guid benzersiz bir isim olması gerektiğinden dolayı kullanıldı.
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "images/" + fileName);
+            var stream = new FileStream(path, FileMode.Create);
+            await file.CopyToAsync(stream);
+            return Created("", file);
+        }
+    }
+}
