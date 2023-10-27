dfd0204e18a15a8969f58eabf9cba7d83a9934b9
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-27 10:02:18 +0300

    Proje seviyesinde Authorize işleminin uygulanması.

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -4,19 +4,35 @@ using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.GuestDto;
 using HotelProject.WebUI.ValidationRules.GuestValidationRules;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddHttpClient();
 
 //FluentValidation un kullanýlmasý için AddFluentValidation metodunun geçilmesi.
 builder.Services.AddTransient<IValidator<CreateGuestDto>, CreateGuestValidator>();
 builder.Services.AddTransient<IValidator<UpdateGuestDto>, UpdateGuestValidator>();
-builder.Services.AddControllersWithViews().AddFluentValidation(); 
-builder.Services.AddAutoMapper(typeof(Program)); //AutoMapper kullanýlmasý için burada belirtmemiz gerekti.
-builder.Services.AddDbContext<Context>();
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
+builder.Services.AddControllersWithViews().AddFluentValidation();
+//------------------------------------------------------------------------------
+
+//AutoMapper kullanýlmasý için burada belirtmemiz gerekti.
+builder.Services.AddAutoMapper(typeof(Program));
+//------------------------------------------------------------------------------
+
+//Proje seviyesinde authorize iþlemi
+builder.Services.AddMvc(config =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+    .RequireAuthenticatedUser()
+    .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+});
+
 
 var app = builder.Build();
 
@@ -30,6 +46,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseAuthentication(); //Proje seviyesinde authorize iþlemi
 
 app.UseRouting();
 
