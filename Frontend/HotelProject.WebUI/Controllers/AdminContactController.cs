c40647d667b36fea50261db2d37972dbfe068fe0
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-28 09:51:56 +0300

    Gelen ve Gönderilen mesaj sayılarının apiden consume edilme işleminin yapılması.

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -22,10 +22,21 @@ namespace HotelProject.WebUI.Controllers
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("http://localhost:50430/api/Contact");
+
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage2 = await client2.GetAsync("http://localhost:50430/api/Contact/GetContactCount");
+
+            var client3 = _httpClientFactory.CreateClient();
+            var responseMessage3 = await client3.GetAsync("http://localhost:50430/api/SendMessage/GetSendMessageCount");
+
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                ViewBag.contactCount = jsonData2;
+                var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+                ViewBag.sendMessageCount = jsonData3;
                 return View(values);
             }
             return View();
@@ -69,10 +80,10 @@ namespace HotelProject.WebUI.Controllers
             return View();
         }
         //Mesaj bölümünde soldaki gelen kutusunun olduğu kısım partialview
-        public PartialViewResult SidebarAdminContactPartial()
-        {
-            return PartialView();
-        }
+        //public PartialViewResult SidebarAdminContactPartial()
+        //{           
+        //    return PartialView();
+        //}
         //Mesaj bölümünde soldaki kategori bölümü partialview
         public PartialViewResult SidebarAdminContactCategoryPartial()
         {
