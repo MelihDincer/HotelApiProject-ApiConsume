5f5f140a210f893c5deca4df5b45289978fa9b85
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-28 11:26:09 +0300

    AppUser tablosuna ImageUrl eklenmesi.

--- ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
@@ -12,5 +12,6 @@ namespace HotelProject.EntityLayer.Concrete
         public string? Name { get; set; }
         public string? Surname{ get; set; }
         public string? City{ get; set; }
+        public string? ImageUrl{ get; set; }
     }
 }
