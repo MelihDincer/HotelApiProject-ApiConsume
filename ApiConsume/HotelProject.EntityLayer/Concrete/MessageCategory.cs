bf79c2a47bc6634c23f178e2f7ea648cff9ec842
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-28 09:47:59 +0300

    MessageCategory entity class ının eklenmesi.

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/MessageCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class MessageCategory
+    {
+        public int MessageCategoryID { get; set; }
+        public string? MessageCategoryName { get; set; }
+
+        //Bire çok ilişki
+        public List<Contact> Contacts { get; set; }
+    }
+}
