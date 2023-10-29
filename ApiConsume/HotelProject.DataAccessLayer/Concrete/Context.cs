a767458dbd13aeeb98f09832dd49f2d39da24953
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-29 12:18:16 +0300

    WorkLocations tablosu eklendi.

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -21,5 +21,6 @@ namespace HotelProject.DataAccessLayer.Concrete
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<SendMessage> SendMessages { get; set; }
         public DbSet<MessageCategory> MessageCategories { get; set; }
+        public DbSet<WorkLocation> WorkLocations { get; set; }
     }
 }
