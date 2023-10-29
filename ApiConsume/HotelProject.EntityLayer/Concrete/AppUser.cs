b545fd1d778e1d107b829b299993ef477113224d
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-29 12:19:57 +0300

    AppUser da yeni bir property eklenip WorkLocations tablosu ile ilişkilendirildi.

--- ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
@@ -13,5 +13,8 @@ namespace HotelProject.EntityLayer.Concrete
         public string? Surname{ get; set; }
         public string? City{ get; set; }
         public string? ImageUrl{ get; set; }
+        public string? WorkDepartment{ get; set; }
+        public int WorkLocationID { get; set; }
+        public WorkLocation WorkLocation { get; set; }
     }
 }
