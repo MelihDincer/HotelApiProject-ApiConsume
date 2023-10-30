928180b8dfbf6d4284b79c0ee590e33d0d5033b0
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-30 16:19:38 +0300

    Personel sayısı ve Son 4 personelin listesi için dataaccess katmanında metot imzalarının tanımlanması.

--- ApiConsume/HotelProject.DataAccessLayer/Abstract/IStaffDal.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Abstract/IStaffDal.cs
@@ -9,5 +9,7 @@ namespace HotelProject.DataAccessLayer.Abstract
 {
     public interface IStaffDal : IGenericDal<Staff>
     {
+        int GetStaffCount();
+        List<Staff> Last4Staff();
     }
 }
