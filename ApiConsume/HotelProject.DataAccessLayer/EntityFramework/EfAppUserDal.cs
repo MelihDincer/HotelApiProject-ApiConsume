e1de0407312f498f175b9501daf9360f654ef73e
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-30 16:15:06 +0300

    EfAppUserDal'ın oluşturulması.

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfAppUserDal.cs
@@ -0,0 +1,27 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfAppUserDal : GenericRepository<AppUser>, IAppUserDal
+    {
+        public EfAppUserDal(Context context) : base(context)
+        {
+        }
+
+        public int GetAppUserCount()
+        {
+            var context = new Context();
+            var value = context.Users.Count();
+            return value;
+        }
+    }
+}
