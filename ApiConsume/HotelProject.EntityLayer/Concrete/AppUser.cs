16b2dcb6d07c5f0d284db39228e07fd70b964bca
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-30 16:21:59 +0300

    AppUser tablosuna ülke ve cinsiyet proplarının eklenmesi.

--- ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
@@ -12,6 +12,8 @@ namespace HotelProject.EntityLayer.Concrete
         public string? Name { get; set; }
         public string? Surname{ get; set; }
         public string? City{ get; set; }
+        public string? Country{ get; set; }
+        public string? Gender{ get; set; } //Cinsiyet
         public string? ImageUrl{ get; set; }
         public string? WorkDepartment{ get; set; }
         public int WorkLocationID { get; set; }
