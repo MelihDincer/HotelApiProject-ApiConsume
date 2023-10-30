ced10a298a9c515b440dc94ba998406e01d85b5a
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-30 16:32:15 +0300

    AppUserController'ın oluşturulması. Kullanıcı sayısı ve kullanıcı listesinin lokasyon bilgileriyle getirilme işlemlerinin tamamlanması.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/AppUserController.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppUserController : ControllerBase
+    {
+        private readonly IAppUserService _appUserService;
+
+        public AppUserController(IAppUserService appUserService)
+        {
+            _appUserService = appUserService;
+        }
+
+        [HttpGet]
+        public IActionResult AppUserList()
+        {
+            Context context = new Context();
+            var values = context.Users.Include(x => x.WorkLocation).Select(y => new AppUserWorkLocationViewModel
+            {
+                Name = y.Name,
+                Surname = y.Surname,
+                WorkLocationID = y.WorkLocationID,
+                WorkLocationName = y.WorkLocation.WorkLocationName,
+                City = y.City,
+                Country = y.Country,
+                Gender = y.Gender,
+                ImageUrl = y.ImageUrl
+            }).ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("GetAppUserCount")]
+        public IActionResult GetAppUserCount()
+        {
+            var value = _appUserService.TGetAppUserCount();
+            return Ok(value);
+        }
+    }
+}
