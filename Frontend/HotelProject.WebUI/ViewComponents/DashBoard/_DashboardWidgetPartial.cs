67ebfe74f149baee9b1affcf925947971bdd2d91
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-31 21:38:55 +0300

    Dashboard widgets bölümünün partial olarak eklenmesi ve ilgili viewcomponent classın oluşturulması. Widgets'da bazı veri sayılarının dinamik olarak entitye özgü metotlar sayesinde gösterilmesi.

---
+++ Frontend/HotelProject.WebUI/ViewComponents/DashBoard/_DashboardWidgetPartial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.ViewComponents.DashBoard
+{
+    public class _DashboardWidgetPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardWidgetPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/Staff/GetStaffCount");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            ViewBag.staffCount = jsonData;
+
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage2 = await client2.GetAsync("http://localhost:50430/api/Booking/GetBookingCount");
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            ViewBag.bookingCount = jsonData2;
+
+            var client3 = _httpClientFactory.CreateClient();
+            var responseMessage3 = await client3.GetAsync("http://localhost:50430/api/AppUser/GetAppUserCount");
+            var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+            ViewBag.appUserCount = jsonData3;
+
+            var client4 = _httpClientFactory.CreateClient();
+            var responseMessage4 = await client4.GetAsync("http://localhost:50430/api/Room/GetRoomCount");
+            var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+            ViewBag.roomCount = jsonData4;
+
+            return View();
+        }
+    }
+}
