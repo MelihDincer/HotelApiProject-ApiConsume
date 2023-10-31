6a309587ed6f323da1ed0e59fea97b4a0a1781ec
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-31 20:38:41 +0300

    Rol atama ve rol ekleme işlemleri için viewmodellerin oluşturulması.

---
+++ Frontend/HotelProject.WebUI/Models/Role/RoleAssignViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelProject.WebUI.Models.Role
+{
+    public class RoleAssignViewModel
+    {
+        public string? RoleName { get; set; }
+        public int RoleID { get; set; }
+        public bool RoleExist { get; set; } //O role sahip mi, evet ise true değil ise false dönecek.
+    }
+}
