0228db120ed4825cc5a43bc283dffe6ea3830419
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-31 21:35:48 +0300

    Dashboardda bulunan son 4 personelin listelendiği ve bu değerin apiden çekildiği viewcomponent class'ın oluşturulması.

---
+++ Frontend/HotelProject.WebUI/ViewComponents/DashBoard/_DashboardLast4StaffList.cs
@@ -0,0 +1,31 @@
+﻿using HotelProject.WebUI.Dtos.StaffDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.ViewComponents.DashBoard
+{
+    public class _DashboardLast4StaffList : ViewComponent
+    {
+        //ViewComponent'te Consume İşlemi
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardLast4StaffList(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/Staff/GetLast4Staff");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLast4StaffDto>>(jsonData);
+                return View(values);
+            }
+            return View();           
+        }
+    }
+}
