c52b38f8579255d7b32baa840fd23a0bc1a3f589
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-31 20:25:43 +0300

    Kullanıcı listesinin apiden consume edilerek listelenmesi.

--- Frontend/HotelProject.WebUI/Controllers/AdminUsersController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminUsersController.cs
@@ -1,22 +1,34 @@
 ﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.AppUser;
+using HotelProject.WebUI.Dtos.RoomDto;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class AdminUsersController : Controller
     {
-        private readonly UserManager<AppUser> _userManager;
 
-        public AdminUsersController(UserManager<AppUser> userManager)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminUsersController(IHttpClientFactory httpClientFactory)
         {
-            _userManager = userManager;
+            _httpClientFactory = httpClientFactory;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> UserList()
         {
-            var values = _userManager.Users.ToList();
-            return View(values);
+            var client = _httpClientFactory.CreateClient(); //İstemci oluşturduk.
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/AppUser/"); //Adrese istekte bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync(); //Gelen veri jsondata değişkenine aktarıldı. jsondata json türünde.
+                var values = JsonConvert.DeserializeObject<List<ResultAppUserDto>>(jsondata); //Tablomuza verinin aktarılması için deserialize işlemi yapıldı.
+                return View(values);
+            }
+            return View();
         }
     }
 }
