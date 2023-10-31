84d53905c4c4035efc4280f43c567b703b02619a
Melih DİNÇER <m_dincer41@hotmail.com>
2023-10-31 21:36:20 +0300

    Dashboardda bulunan son 6 rezervasyonun  listelendiği ve bu değerin apiden çekildiği viewcomponent class'ın oluşturulması.

---
+++ Frontend/HotelProject.WebUI/ViewComponents/DashBoard/_DashboardLast6Booking.cs
@@ -0,0 +1,31 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.StaffDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.ViewComponents.DashBoard
+{
+    public class _DashboardLast6Booking : ViewComponent
+    {
+        //ViewComponent'te Consume İşlemi
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardLast6Booking(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:50430/api/Booking/Last6Booking");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLast6BookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
